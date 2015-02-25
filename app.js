e64f7f11fc01afa2e76b0c2851dbe76f814ad27f
wj <w773679148@163.com>
2015-02-25 20:07:11 +0800

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +24,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/', routes.login);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.logout);
+app.get('/home', routes.home);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+
+router.index = function(req, res){
+res.render('index', { title: 'Index' });
+};
+router.login = function(req, res){
+res.render('login', { title: '用户登陆'});
+};
+router.doLogin = function(req, res){
+var user={
+username:'admin',
+password:'admin'
+}
+if(req.body.username===user.username && req.body.password===user.password){
+res.redirect('/home');
+}
+res.redirect('/login');
+};
+router.logout = function(req, res){
+res.redirect('/');
+};
+router.home = function(req, res){
+var user={
+username:'admin',
+password:'admin'
+}
+res.render('home', { title: 'Home',user: user});
+};
 
 module.exports = router;
